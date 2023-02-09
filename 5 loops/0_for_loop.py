05f4acea0d5cbf6426e16b9fbf8b891797eb9a97
Pirate30 <53134657+Pirate30@users.noreply.github.com>
2023-02-09 14:01:29 +0530

    basics of for loop and range function

---
+++ 5 loops/0_for_loop.py
@@ -0,0 +1,6 @@
+# for n in range(5):
+#     print(n)
+# for n in range(1,5):
+#     print(n)
+for n in range(1,10,2):
+    print(n)
---
+++ 5 loops/range.py
@@ -0,0 +1,23 @@
+range(5)
+# starts with 0
+# condition -> runs until the iteration count < given number(5)
+# increaments -> 1 at an interation
+# => 0  1 2 3 4 
+
+range(1,5)
+# starts with 1
+# condition => iteration count < 5
+# inc: +=1
+# => 1 2 3 4
+
+range(1,5,2)
+# starts with 1
+# condition => iteration count < 5
+# inc: +=2
+# => 1  3
+
+
+range(10,0,-1)
+# starts with 10
+# condition => iter count < 0
+# decr: -1

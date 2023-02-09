5e8febaa9714a1164f5f2099c865a9ce723ed144
Pirate30 <53134657+Pirate30@users.noreply.github.com>
2023-02-09 11:14:20 +0530

    basic of operators

---
+++ 1 operators/p0.py
@@ -0,0 +1,122 @@
+# arithmatic operators
+a=15
+b=4
+# addition
+# print('+',a+b)
+# subtraction -
+# print('-',a-b)
+
+# multi *
+# print('*',a*b)
+
+# div /
+# print('/',a/b)
+
+# modulus %
+# print('%',a%b)
+
+# exponents **
+# print('**',a**b)
+
+# floor division //
+# print('//',a//b)
+
+
+# --------------------------------------------------------------------
+# assignment operators
+# =
+# x=5
+# +=
+# x+=2
+# print(x)
+# -=
+# x-=2
+# print(x)
+
+# ---------------------------------------------------------------------
+# comparison operators
+x=5
+y=6
+z=5
+# equal ==
+# print(x==x)
+# print(x==y)
+# print(x==z)
+# not equal !=
+# print(x!=y)
+# print(x!=z)
+# greater >
+# print(x>y)
+# print(x>z)
+# print(x>x)
+# print(y>x)
+# lesser <
+# print(x<y)
+# print(x<x)
+# print(x<z)
+# greater or equal >=
+# print(x>=x)
+# print(x>=y)
+# print(x>=z)
+# print(y>=x)
+# lesser ot equal <=
+# print(x<=x)
+# print(x<=y)
+# print(x<=z)
+# print(y<=x)
+
+# ----------------------------------------------------------------
+# logical operators
+x=10
+y=20
+# and
+# print(x==10 and x<y)
+# or
+# print(x==10 or x>y)
+# not
+# print(not(x!=10))
+
+# -----------------------------------------------------------------
+# membership operators
+x='welcome to hell'
+y=[10,20,'hello',40, False]
+#=> in
+# print('h' in x)
+# print(30 in y)
+#=> not in
+# print('H' not in x)
+# print(True not in y)
+# print(False not in y)
+
+# -------------------------------------------------------------------
+# identity operators
+x=10
+y=20
+z=10
+# => is - same as ==
+# print(x is x)
+# print(x is y)
+# print(x is z)
+# => is not - same as !=
+# print(x is not x)
+# print(x is not y)
+# print(x is not z)
+
+# --------------------------------------------------------------------
+# bitwise operators
+#? truth table
+# ? a  b  a&b  a|b  a^b
+# ? 0  0   0    0    0
+# ? 0  1   0    1    1
+# ? 1  0   0    1    1
+# ? 1  1   1    1    0
+x=10
+y=11
+# & - and => 1 if both are 1 else 0
+# print(bin(x), "number is", x)
+# print(bin(y), "number is", y)
+# print(bin(x&y),"binary to number is =>", x&y)
+# | - or => 0 if both are 0 else 1
+# print(bin(x|y),"binary to number is =>", x|y)
+# ^ - xor => 0 if both are same else 1
+# print(bin(x^y),"binary to number is =>", x^y)

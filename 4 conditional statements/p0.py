0c5aa92ab47dcabeb10fab05a401f5f3574a1c01
Pirate30 <53134657+Pirate30@users.noreply.github.com>
2023-02-09 12:33:20 +0530

    basics of conditional statements

---
+++ 4 conditional statements/p0.py
@@ -0,0 +1,39 @@
+# condition based statememts allows us to control the flow of execution in your code based on certain conditions
+
+# #? types
+    # if
+    # if else
+    # if elif
+
+# if
+a=11
+
+# if a>5:
+#     print('a is greated than 5')
+# if a>10:
+#     print('a is greater than 10')
+# if a%2==0:
+#     print('a is even number')
+# if(a%2!=0):
+#     print('a is odd number')
+
+# -------------------------------------------------------------------
+
+# if else
+# a=eval(input('enter the value:'))
+# if a%2==0:
+#     print(a,'is even')
+# else:
+#     print(a, 'is odd')
+
+# ---------------------------------------------------------------------
+
+# if elif else
+x=eval(input('enter the value:'))
+
+if x>0:
+    print(x,'is positive')
+elif x<0:
+    print(x,'is negative')
+else:
+    print(x,'is 0')

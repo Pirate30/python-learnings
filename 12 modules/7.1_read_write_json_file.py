4ed56ae4535a3e10334913bfe66c93b12b50f170
Pirate30 <53134657+Pirate30@users.noreply.github.com>
2023-02-26 11:07:55 +0530

    tests

--- 12 modules/7.1_read_write_json_file.py
+++ 12 modules/7.1_read_write_json_file.py
@@ -1,9 +1,13 @@
 import json
 
 file = open('./12 modules/json.json', 'r')
-# r - mode - read
+#! r - mode - read
 data = file.read()  # this data will be in string format
 final = json.loads(data)  # loads will convert that into python object
-print(final)
-print(type(data))  # <class 'str'>
-print(type(final))  # <class 'list'>
+# print(final)
+# print(type(data))  # <class 'str'>
+# print(type(final))  # <class 'list'>
+
+for a in final:
+    print(a['serviceId'], a['serviceName'])
+    print('------------------')
